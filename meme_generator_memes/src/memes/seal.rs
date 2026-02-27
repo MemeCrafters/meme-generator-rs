30e0c263eb489d395ed7705efded3db57eb9b828
meetwq <meetwq@gmail.com>
2026-02-27 16:19:21 +0800

    fix(meme): fix `seal`

--- meme_generator_memes/src/memes/seal.rs
+++ meme_generator_memes/src/memes/seal.rs
@@ -4,19 +4,19 @@ use meme_generator_core::error::Error;
 use meme_generator_utils::{
     builder::InputImage,
     encoder::make_png_or_gif,
-    image::{Fit, ImageExt},
+    image::ImageExt,
     tools::{load_image, local_date},
 };
 
 use crate::{options::NoOptions, register_meme, tags::MemeTags};
 
 fn seal(images: Vec<InputImage>, _: Vec<String>, _: NoOptions) -> Result<Vec<u8>, Error> {
-    let size = images[0].image.dimensions();
+    let width = images[0].image.square().width();
     let mask = load_image("seal/0.png")?;
-    let mask = mask.resize_fit(size, Fit::Contain);
+    let mask = mask.resize_exact((width, width));
 
     let func = |images: Vec<Image>| {
-        let img = &images[0];
+        let img = &images[0].square();
         let mut surface = img.to_surface();
         let canvas = surface.canvas();
         canvas.draw_image(&mask, (0, 0), None);
